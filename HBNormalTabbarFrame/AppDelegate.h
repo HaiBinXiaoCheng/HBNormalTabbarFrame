e6f8e3d7df9e995138ddbcc7031392029aede28d
pengbin <pengbin0708@163.com>
2017-11-27 09:54:40 +0800

    Initial Commit

---
+++ HBNormalTabbarFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBNormalTabbarFrame
+//
+//  Created by mass on 2017/11/27.
+//  Copyright © 2017年 HB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBNormalTabbarFrame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HBNormalTabbarFrame
+//
+//  Created by mass on 2017/11/27.
+//  Copyright © 2017年 HB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
